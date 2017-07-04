d8616c4336ea4f5257bde01a86598e12ec989787
masterUNG <phrombutr@gmail.com>
2017-07-04 09:12:43 +0700

    Success

--- Menu and Tool Bar/ViewController.swift
+++ Menu and Tool Bar/ViewController.swift
@@ -9,6 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var intNumber: Int = 0
+    
+    
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func backButtonItem(_ sender: Any) {
+        
+        intNumber -= 1
+        changeNumber(numChange: intNumber)
+        
+    }
+    
+    @IBAction func resetButtonItem(_ sender: Any) {
+        
+        intNumber = 0
+        changeNumber(numChange: intNumber)
+        
+    }
+    
+    @IBAction func nextButtonItem(_ sender: Any) {
+        
+        intNumber += 1
+        changeNumber(numChange: intNumber)
+        
+    }
+    
+    
+    func changeNumber(numChange: Int) -> Void {
+        numberLabel.text = String(numChange)
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
